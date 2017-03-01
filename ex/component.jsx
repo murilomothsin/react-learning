f79f0d3083ccf797c687fbf711bcf23384c8e315
Murilo Mothsin <mothsin@hotmail.com>
2017-03-01 15:27:43 -0300

    Exportando mais de um componente em um único arquivo

--- ex/component.jsx
+++ ex/component.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 
-export default () => (
-    <h1>Primeiro Componente</h1>
+export default props => (
+    <h1>Primeiro Component!</h1>
 )
+
+export const Segundo = props => (
+    <h1>Segundo Component!</h1>
+)
+
+// export { Primeiro, Segundo }
