bc78ee333b828bcf4787a89a32643c5c29ac96cc
Murilo Mothsin <mothsin@hotmail.com>
2017-08-17 17:50:12 -0300

    Criar aplicação contador com Redux

--- ex.1/index.jsx
+++ ex.1/index.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { combineReducers, createStore } from 'redux'
 import { Provider } from 'react-redux'
-import CounterReducer from './counterReducer'
-import Counter from './counter'
+import Field from './field'
+import fieldReducer from './fieldReducer'
 
 const reducers = combineReducers({
-    field: CounterReducer
+    field: fieldReducer
 })
 
 ReactDOM.render(
     <Provider store={createStore(reducers)}>
-        <Counter />
+        <Field initialValue='Teste' />
     </Provider>
     , document.getElementById('app'))
